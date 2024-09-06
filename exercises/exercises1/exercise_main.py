1054882f84d045e9761104fe1bffc15240048c01
Alexander Musihin <alexander.mvae@gmail.com>
2024-09-06 07:44:26 +0200

    changed task main

--- exercises/exercises1/exercise_main.py
+++ exercises/exercises1/exercise_main.py
@@ -3,15 +3,5 @@ from exercise_classes import Report,Contract,Presentation
 
 if __name__ == "__main__":
     
-    report_template = Report("Annual Report","Content of the annual report.","Alex Mus")
-    contract_template = Contract("Rent Contract","Content of the Rent Contract","Alex Mus")
-    presentation_template = Presentation("Projectpresentation", "Content of Presentation", "Alex Mus")
-
-    print(report_template)
-    print(contract_template)
-    print(presentation_template)
+    print("Hello World!")
     
-
-    new_doc = report_template.clone()
-    new_doc.set_type("Presentation")
-    print(new_doc)
